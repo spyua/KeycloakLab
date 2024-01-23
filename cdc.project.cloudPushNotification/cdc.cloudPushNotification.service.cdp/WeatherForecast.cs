b690aa6c4e2dfb248f4276fa0bed3af79a397803
pal2097 <pal2097@gmail.com>
2024-01-23 16:42:17 +0800

    feat:upload test project

---
+++ cdc.project.cloudPushNotification/cdc.cloudPushNotification.service.cdp/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace cdc.cloudPushNotification.service.cdp
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
