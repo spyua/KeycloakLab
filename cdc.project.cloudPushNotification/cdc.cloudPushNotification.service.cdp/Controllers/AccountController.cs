2601aa2099f1c2946df6446d1f1f711f20126b4e
pal2097 <pal2097@gmail.com>
2024-01-24 17:21:40 +0800

    WIP

---
+++ cdc.project.cloudPushNotification/cdc.cloudPushNotification.service.cdp/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cdc.cloudPushNotification.service.cdp.Controllers
+{
+
+    [ApiController]
+    [Route("api/user")]
+    public class AccountController : ControllerBase
+    {
+
+        private readonly ILogger<AccountController> _logger;
+
+        public AccountController(ILogger<AccountController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Authorize]
+        [HttpGet(nameof(Login))]
+        public async Task<string> Login()
+        {
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            var idToken = await HttpContext.GetTokenAsync("id_token");
+            var refreshToken = await HttpContext.GetTokenAsync("refresh_token");
+
+            return "auth check ok";
+        }
+
+    }
+}
--- cdc.project.cloudPushNotification/cdc.cloudPushNotification.service.cdp/Program.cs
+++ cdc.project.cloudPushNotification/cdc.cloudPushNotification.service.cdp/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,35 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+})
+.AddCookie(options =>
+{
+    options.LoginPath = "/Account/Login";
+
+  
+})
+.AddOpenIdConnect(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.Authority = "http://localhost:8082/realms/api-role-lab";
+    options.ClientId = "admin-rest-client";
+    options.ClientSecret = "FJ0LErju9eRJsu4tZMXiOCrq2pfh5AZt";
+    options.ResponseType = "code";
+    options.Scope.Add("openid");
+    options.Scope.Add("profile");
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        NameClaimType = "preferred_username",
+        RoleClaimType = "roles"
+    };
+});
 
 var app = builder.Build();
 
